542c2cf05b7e28bb7d9b37bc675d970a8e3a32c0
EinKaktus <217721628+simonsarcevic@users.noreply.github.com>
2026-03-30 21:41:19 +0200

    changing router links

--- src/sites/privacy-policy/privacy-policy.ts
+++ src/sites/privacy-policy/privacy-policy.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {Cursor} from '../../design/cursor/cursor';
 import {Footer} from '../../components/footer/footer';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [Cursor, Footer],
+  imports: [Cursor, Footer, RouterLink],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.css',
 })
