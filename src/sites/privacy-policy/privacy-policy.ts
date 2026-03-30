14680320c1cca9794228b3f69719f95b55d2aafa
EinKaktus <217721628+simonsarcevic@users.noreply.github.com>
2026-03-30 21:34:36 +0200

    adding privacy policy

--- src/sites/privacy-policy/privacy-policy.ts
+++ src/sites/privacy-policy/privacy-policy.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {Cursor} from '../../design/cursor/cursor';
+import {Footer} from '../../components/footer/footer';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [],
+  imports: [Cursor, Footer],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.css',
 })
