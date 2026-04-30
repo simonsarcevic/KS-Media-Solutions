71aee87d6a6b242461aa024cf3787504d1154f79
EinKaktus <217721628+simonsarcevic@users.noreply.github.com>
2026-04-30 10:20:09 +0200

    removed imprint and privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
-import {Imprint} from '../sites/imprint/imprint';
-import {PrivacyPolicy} from '../sites/privacy-policy/privacy-policy';
 import {MainSite} from '../sites/main-site/main-site';
 
 export const routes: Routes = [
   {path: '', component: MainSite},
-  {path: 'imprint', component: Imprint},
-  {path: 'privacy-policy', component: PrivacyPolicy}
 ];
