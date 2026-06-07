7e87db69d10349b0d8e8cc1519efdd6a81916922
EinKaktus <217721628+simonsarcevic@users.noreply.github.com>
2026-06-07 02:16:12 +0200

    adding imprint and privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import {MainSite} from '../sites/main-site/main-site';
+import {PrivacyPolicy} from '../sites/privacy-policy/privacy-policy';
+import {Imprint} from '../sites/imprint/imprint';
 
 export const routes: Routes = [
   {path: '', component: MainSite},
+  {path: 'privacy-policy', component: PrivacyPolicy},
+  {path: 'imprint', component: Imprint}
 ];
